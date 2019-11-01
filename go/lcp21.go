eb0d8e97b0e271b55bdb7e6b93f0f8f41bd3e03f
zhangxuanru <1107099011@qq.com>
2019-11-01 16:55:45 +0800

    add lettcode-cn 804

--- go/lcp21.go
+++ go/lcp21.go
@@ -6,10 +6,10 @@
 */
 package _go
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
 
 /*
 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
