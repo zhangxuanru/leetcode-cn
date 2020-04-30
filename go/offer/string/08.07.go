a223091b606249dc6d454318e0db0746d5ae84e5
zhangxuanru <1107099011@qq.com>
2020-04-30 17:36:35 +0800

    08-07

--- go/offer/string/08.07.go
+++ go/offer/string/08.07.go
@@ -1,5 +1,9 @@
 package string
 
+import (
+	"fmt"
+)
+
 /**
 无重复字符串的排列组合。编写一种方法，计算某字符串的所有排列组合，字符串每个字符均不相同。
 
@@ -16,4 +20,53 @@ func Permutation(S string) []string {
 	var strList = []string{S}
 
 	return strList
+}
+
+//success
+func Permutation2(S string) []string {
+	n := len(S)
+	ret := []string{}
+	visited := map[int]bool{}
+	var br func(now string, i int)
+
+	br = func(now string, k int) {
+		fmt.Println("params:", now, "i=", k)
+		if len(now) == n {
+			ret = append(ret, now)
+			fmt.Println("now visited:", visited, "i=", k)
+			return
+		}
+		for i := 0; i < n; i++ {
+			if visited[i] {
+				continue
+			}
+			visited[i] = true
+			br(now+string(S[i]), i)
+			visited[i] = false
+			fmt.Println("i=", i, "visited:", visited)
+		}
+	}
+	br("", 0)
+	return ret
+}
+
+//success
+func Permutation3(str []rune, start int) {
+	if str == nil {
+		return
+	}
+	if start == len(str)-1 {
+		fmt.Println(string(str))
+	} else {
+		for i := start; i < len(str); i++ {
+			SwapRune(str, start, i)
+			Permutation3(str, start+1)
+			SwapRune(str, start, i)
+		}
+	}
+}
+func SwapRune(data []rune, x, y int) {
+	tmp := data[x]
+	data[x] = data[y]
+	data[y] = tmp
 }
